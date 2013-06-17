70a0104af1d30f5c64500546eabe22fadb03d82e
OuadiaGamnrane <ouadia.g@outlook.com>
2013-06-17 15:13:55 +0100

    repos Correction

--- src/main/java/com/gcom/repos/AssignmentRepos.java
+++ src/main/java/com/gcom/repos/AssignmentRepos.java
@@ -3,8 +3,10 @@ package com.gcom.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gcom.beans.Assignment;
+
 
 @Repository("assignmentRepos")
-public interface AssignmentRepos  extends JpaRepository<AssignmentRepos, Integer>{
+public interface AssignmentRepos  extends JpaRepository<Assignment, Integer>{
 
 }
