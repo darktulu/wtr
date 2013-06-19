f35fd88758699cec2f51b6886314400f5d9c8e3c
OuadiaGamnrane <ouadia.g@outlook.com>
2013-06-19 16:38:45 +0100

    cleaning interfaces, adding assignment

--- src/main/java/com/gcom/repos/AssignmentRepos.java
+++ src/main/java/com/gcom/repos/AssignmentRepos.java
@@ -1,7 +1,11 @@
 package com.gcom.repos;
 
+import java.util.List;
+
 import com.gcom.beans.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssignmentRepos extends JpaRepository<Assignment, Integer> {
+	
+	List<Assignment> findByUserUsername(String username);
 }
