2d28454b417e8203a9a282851936be18a9d08ba7
OuadiaGamnrane <ouadia.g@outlook.com>
2013-06-20 21:31:33 +0100

    Adding Assignment

--- src/main/java/com/gcom/service/metier/impl/EGeneralDataServiceImpl.java
+++ src/main/java/com/gcom/service/metier/impl/EGeneralDataServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gcom.service.metier.impl;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.dozer.DozerBeanMapper;
@@ -7,9 +9,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gcom.beans.EGeneralData;
+import com.gcom.beans.status.Status;
 import com.gcom.repos.EGeneralDataRepos;
 import com.gcom.service.exceptions.ProfileNotExistException;
 import com.gcom.service.metier.EGeneralDataService;
+import com.gcom.service.utils.ObjectToVOListConverter;
 import com.gcom.view.vo.EGeneralDataVO;
 
 @Service("eGeneralDataService")
@@ -17,16 +21,24 @@ public class EGeneralDataServiceImpl implements EGeneralDataService {
 	@Inject
 	public EGeneralDataRepos eGeneralDataRepos;
 	@Inject
-    private DozerBeanMapper mapper;
-	
+	private DozerBeanMapper mapper;
+
+	@Inject
+	private ObjectToVOListConverter<EGeneralData, EGeneralDataVO> egenConverter;
+
 	@Override
 	@Transactional(readOnly = true)
-	public EGeneralDataVO findById(String username) throws ProfileNotExistException{
+	public EGeneralDataVO findById(String username) throws ProfileNotExistException {
 		EGeneralData employee = eGeneralDataRepos.findOne(username);
 		if (employee == null) {
-            throw new ProfileNotExistException();
-        }
-		
+			throw new ProfileNotExistException();
+		}
+
 		return mapper.map(employee, EGeneralDataVO.class);
+	}
+
+	@Override
+	public List<EGeneralDataVO> loadActiveEmployees() {
+		return egenConverter.convert(eGeneralDataRepos.findByStatus(Status.ACTIVE.getValue()),EGeneralDataVO.class);
 	}
 }
